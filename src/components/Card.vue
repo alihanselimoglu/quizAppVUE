0c98a7bcf87fc43cc3c180e0ae99d960d13ed967
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-24 18:09:34 +0300

    final commit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,48 +1,47 @@
 <script setup>
-import { defineProps } from 'vue';
-import { useRouter } from 'vue-router';
+    import {defineProps} from "vue"
+    import {useRouter} from "vue-router"
 
-const router = useRouter();
+    const router = useRouter()
+    const {quiz} = defineProps(['quiz'])
 
-const { quiz } = defineProps(['quiz']);
-
-const navigateToQuiz = (quiz) => {
-  router.push(`/quiz/${quiz.id}`);
-};
+    const navigateToQuiz = () => {
+      router.push(`/quiz/${quiz.id}`)
+    }
 </script>
 
 <template>
-  <div class="card" @click="navigateToQuiz">
-    <img :src="quiz.img" alt="card image" />
-    <div class="card-text">
-      <h2>{{ quiz.name }}</h2>
-      <p>{{ quiz.questions.length }} Questions</p>
+    <div class="card" @click="navigateToQuiz">
+        <img :src="quiz.img" alt="">
+        <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>{{ quiz.questions.length }} questions</p>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
+    .card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
+    margin-bottom: 35px;
+    margin-right: 20px;
+    cursor: pointer;
+  }
 
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
+  .card img {
+    width: 100%;
+    height: 190px;
+    margin: 0
+  }
 
-.card .card-text {
-  padding: 0 5px;
-}
+  .card .card-text {
+    padding: 0 5px
+  }
 
-.card .card-text h2 {
-  font-weight: bold;
-}
+  .card .card-text h2 {
+    font-weight: bold;
+  }
 </style>
--- src/views/QuizesView.vue
+++ src/views/QuizesView.vue
@@ -1,84 +1,78 @@
 <script setup>
-import q from '../data/quizes.json';
-import { ref, watch } from 'vue';
-import Card from '../components/Card.vue';
+    import q from "../data/quizes.json"
+    import {ref, watch} from "vue"
+    import Card from "../components/Card.vue"
+    import gsap from "gsap"
+  
+    const quizes = ref(q)
+    const search = ref("")
+  
+    watch(search, () => {
+      quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+    })
 
+    const beforeEnter = (el) => {
+      el.style.transform = "translateY(-60px)"
+      el.style.opacity = 0;
+    }
 
-const quizes = ref(q);
-const search = ref('');
-
-
-watch(search, () => {
-  quizes.value = q.filter((quiz) =>
-    quiz.name.toLowerCase().includes(search.value.toLowerCase())
-  );
-});
-
-
-
-</script>
-
-<template>
+    const enter = (el) => {
+      gsap.to(el, {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        delay: el.dataset.index * 0.2
+      })
+    }
+  
+  </script>
+  
+  <template>
     <div>
-        <div>
-    <header>
-      <h1>Quizes</h1>
-      <input v-model="search" type="text" placeholder="Search..." />
-    </header>
-    <div class="options-container">
-     <Card  v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+      <header>
+        <h1>Quizes</h1>
+        <input v-model.trim="search" type="text" placeholder="Search...">
+      </header>
+      <div class="options-container">
+        <TransitionGroup 
+          @before-enter="beforeEnter"
+          @enter="enter"
+          appear
+        >
+          <Card v-for="(quiz, index) in quizes" :key="quiz.id" :quiz="quiz" :data-index="index" />
+        </TransitionGroup>
+      </div>
     </div>
-  </div>
-    </div>
-</template>
+  </template>
+  
+  <style scoped>
 
-<style>
-
-header {
-  margin-bottom: 10px;
-  margin-top: 30px;
-  display: flex;
-  align-items: center;
-}
-
-header h1 {
-  font-weight: bold;
-  margin-right: 30px;
-}
-
-header input {
-  border: none;
-  background-color: rgba(128, 128, 128, 0.1);
-  padding: 10px;
-  border-radius: 5px;
-}
-
-.options-container {
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 40px;
-}
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
-}
- </style>
+  
+    header {
+      margin-bottom: 10px;
+      margin-top: 30px;
+      display: flex;
+      align-items: center;
+    }
+  
+    header h1 {
+      font-weight: bold;
+      margin-right: 30px;
+    }
+  
+    header input {
+      border: none;
+      background-color: rgba(128,128,128,0.1);
+      padding: 10px;
+      border-radius: 5px;
+    }
+  
+    .options-container {
+      display: flex;
+      flex-wrap: wrap;
+      margin-top: 40px;
+    }
+  
+  
+    
+  </style>
